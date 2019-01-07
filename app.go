be93201f8fc1dd0edfc04d7df47a392251ab4b21
Shane Howearth <shane.h.1@gmail.com>
2019-01-07 17:35:13 +1100

    Create routes for app

---
+++ app.go
@@ -0,0 +1,56 @@
+// app.go
+
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+// App -
+type App struct {
+	Router *mux.Router
+	DB     *sql.DB
+}
+
+// Initialise -
+func (a *App) Initialise(user, password, dbname string) {
+	connectionString :=
+		fmt.Sprintf("user=%s password=%s dbname=%s", user, password, dbname)
+
+	var err error
+	a.DB, err = sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	a.Router = mux.NewRouter()
+	a.initialiseRoutes()
+}
+
+// Run -
+func (a *App) Run(addr string) {
+	log.Fatal(http.ListenAndServe(":8000", a.Router))
+}
+
+func (a *App) initialiseRoutes() {
+	a.Router.HandleFunc("/articles/{id:[0-9]+}", a.getArticle).Methods("GET")
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
